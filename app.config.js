2ae670d9adfd2ca973946c94b7b8361e9a2a6d67
PAJK87 <pajk87@gmail.com>
2023-03-23 21:18:49 -0400

    Config Update

--- app.config.js
+++ app.config.js
@@ -6,27 +6,12 @@ export default{
     "slug": "firebase-login-demo",
     "version": "1.0.0",
     "orientation": "portrait",
-    "icon": "../assets/Quizzy-Logo.png",
     "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
-    },
     "assetBundlePatterns": [
       "**/*"
     ],
     "ios": {
       "supportsTablet": true
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
-    },
-    "web": {
-      "favicon": "./assets/favicon.png"
     },
     "extra": {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
